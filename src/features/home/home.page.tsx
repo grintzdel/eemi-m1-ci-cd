2c8875376086dc2cbefef39dba7ae2ad52742425
maoudin <128369158+grintzdel@users.noreply.github.com>
2026-01-15 10:47:04 +0100

    chore: 1 project setup (#12)
    * chore: add lint and format (#7)
    
    * chore: add eslint and prettier with plugins
    
    * chore: add lint and pretty scripts
    
    * chore: add prettier and eslint config file
    
    * chore: add test (#8)
    
    * chore: add tests packages and scripts
    
    * chore: add vitest config files
    
    * chore: add lint workflow file (#9)
    
    * chore: add lint workflow file
    
    * fix: use pnpm instead of npm
    
    * chore: setup cd (#10)
    
    * chore: add deploy workflow file
    
    * chore: add deploy ansible and ini file
    
    * chore: add main ansible playbook file
    
    * chore: setup app file (#11)
    
    * chore: format app files
    
    * chore: update gitignore file
    
    * feat: add default line height
    
    * feat: add layout header and footer
    
    * feat: add base app achitecture

---
+++ src/features/home/home.page.tsx
@@ -0,0 +1,10 @@
+import { HeroSection } from '@/modules/app/react/sections/hero.section'
+import React from 'react'
+
+export default function HomePage(): React.JSX.Element {
+  return (
+    <React.Fragment>
+      <HeroSection />
+    </React.Fragment>
+  )
+}
---
+++ src/modules/app/react/sections/hero.section.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HeroSection: React.FC = (): React.JSX.Element => {
+  return (
+    <section className="hero-section">
+      <h1>Welcome to the Hero Section</h1>
+    </section>
+  )
+}
