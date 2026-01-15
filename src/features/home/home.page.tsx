05b17a71124522114a368c44d01cd413ea0b6565
maoudin <mathis.oudin42@gmail.com>
2026-01-15 16:31:42 +0100

    feat: add base marquee section

--- src/features/home/home.page.tsx
+++ src/features/home/home.page.tsx
@@ -1,10 +1,12 @@
 import { HeroSection } from '@/modules/app/react/sections/hero.section'
+import { MarqueeSection } from '@/modules/app/react/sections/marquee.section'
 import React from 'react'
 
 export default function HomePage(): React.JSX.Element {
   return (
     <React.Fragment>
       <HeroSection />
+      <MarqueeSection />
     </React.Fragment>
   )
 }
---
+++ src/modules/app/react/sections/marquee.section.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MarqueeSection: React.FC = (): React.JSX.Element => {
+  return (
+    <section>
+      <div>Marquee section</div>
+    </section>
+  )
+}
